d650f58c4694bff1e6b6cc43232903da2725b563
Deekshith286 <deekshithreddybojjala004@gmail.com>
2025-09-13 14:02:58 +0530

    Delete src directory

--- src/Title.jsx
+++
@@ -1,8 +0,0 @@
-function Title(props) {
-    return (
-        <h1>
-            Hi, {props.text}
-        </h1>
-    )
-}
-export default Title
--- src/button.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-const Button=(props)=>{
-    return <div>I am Dummy</div>
-        
-}
-
-export default Button;
